c1469e437d8e0fd4dd3cb58f32441c46db382d34
MalgorzataIzdebska <m.izdebska1998@gmail.com>
2022-06-11 20:45:55 +0200

    bazy danych 3

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import csv, sqlite3
 
+if __name__ == "__main__":
+    file = input()
+
 class ReportGenerator:
+
     def __init__(self,connection, escape_string="(%s)"):
         self.connection=connection
         self.report_text=None
         self.escape_string=escape_string
-
-    def get_report(self):
-        return self.report_text
 
     def generate_report(self):
         cursor=self.connection.cursor()
         sql_query=f"SELECT sum(duration) FROM polaczenia"
         cursor.execute(sql_query)
         result=cursor.fetchone()[0]
-        self.report_text=result   
+        self.report_text=result
+
+    def get_report(self):
+        return self.report_text
 
 sqlite_con = sqlite3.connect(':memory:', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
 cur = sqlite_con.cursor()
@@ -24,6 +28,3 @@ cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER,
                   datetime data_type timestamp, 
                   duration data_type INTEGER , 
                   celltower data_type INTEGER);''')
-
-if __name__ == "__main__":
-    file = input()
